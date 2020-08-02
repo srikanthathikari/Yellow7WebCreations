21d41c8d3546f546232ccbd59e32aae3a45720cd
Srikanth <srikantathikari@gmail.com>
2020-08-02 14:38:20 -0400

    Update html and index.js with nodemailer and form with nam attributes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var nodemailer = require('nodemailer');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,8 +14,34 @@ app.get('/', function(req, res){
 })
 
 app.post('/serverTest', function(req, res) {
+    console.log(req.body)
     res.send('You ve reached the post page '+ req.body);
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'email',
+          pass: 'password'
+        }
+      });
+      
+      var mailOptions = {
+        from: 'srikantathikari@gmail.com',
+        to: 'yogitareddyb@gmail.com',
+        subject: 'Sending Email for our first Order',
+        text: req.body.FirstName+' has placed an order, you can reach '+ req.body.FirstName+' @ '+ req.body.PhoneNumber
+      };
+
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
   });
+
+  
 
 app.listen(8080, function () {
     console.log('Server Started');
